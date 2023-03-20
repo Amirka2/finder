1e7cf9ae8e25ecfe8dcacca3d722e3c52a078cea
Amir <70907990+Amirka2@users.noreply.github.com>
2023-03-20 21:21:39 +0500

    added user class

---
+++ src/main/java/com/example/model/User.java
@@ -0,0 +1,33 @@
+package com.example.model;
+
+public class User {
+    private final String login;
+    private String password;
+    private String email;
+
+    public User(String login, String password, String email) {
+        this.login = login;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
